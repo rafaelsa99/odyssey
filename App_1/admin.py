17532a7ca0c7ee25927f3ec7d37da43334b4a7d4
rafaelsa99 <rafa.sa_@hotmail.com>
2022-05-15 22:37:37 +0100

    Register model in admin

--- App_1/admin.py
+++ App_1/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 # Register your models here.
+from .models import Attribute, AttributeCategory, AttributeChoice, Metric, MetricType, Occurrence, Site, File
+
+admin.site.register(Site, admin.ModelAdmin)
+admin.site.register(Occurrence, admin.ModelAdmin)
+admin.site.register(File, admin.ModelAdmin)
+admin.site.register(MetricType, admin.ModelAdmin)
+admin.site.register(Metric, admin.ModelAdmin)
+admin.site.register(AttributeCategory, admin.ModelAdmin)
+admin.site.register(AttributeChoice, admin.ModelAdmin)
+admin.site.register(Attribute, admin.ModelAdmin)
