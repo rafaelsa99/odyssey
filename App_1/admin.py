a6a9e9157646ba5d60edeec5a7579890c4aa6d3d
rafaelsa99 <rafa.sa_@hotmail.com>
2022-05-16 01:15:26 +0100

    Custom GISModelAdmin

--- App_1/admin.py
+++ App_1/admin.py
@@ -3,8 +3,18 @@ from django.contrib.gis import admin
 # Register your models here.
 from .models import Attribute, AttributeCategory, AttributeChoice, Metric, MetricType, Occurrence, Site, File
 
-admin.site.register(Site, admin.ModelAdmin)
-admin.site.register(Occurrence, admin.ModelAdmin)
+class CustomGeoWidgetAdmin(admin.GISModelAdmin):
+    """Custom GISModelAdmin with default coordinates to Portugal."""
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_zoom': 6,
+            'default_lon': -8.130229,
+            'default_lat': 39.694819,
+        },
+    }
+
+admin.site.register(Site, CustomGeoWidgetAdmin)
+admin.site.register(Occurrence, CustomGeoWidgetAdmin)
 admin.site.register(File, admin.ModelAdmin)
 admin.site.register(MetricType, admin.ModelAdmin)
 admin.site.register(Metric, admin.ModelAdmin)
