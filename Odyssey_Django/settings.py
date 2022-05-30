23ff6ab0212712ed198761f208bd34def2855a84
rafaelsa99 <rafa.sa_@hotmail.com>
2022-05-30 22:32:05 +0100

    Rename App

--- Odyssey_Django/settings.py
+++ Odyssey_Django/settings.py
@@ -72,7 +72,7 @@ INSTALLED_APPS = [
 
     #Custom Apps
     'landing_app',
-    'App_1',
+    'sdi',
 
     ]
 
@@ -159,6 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
