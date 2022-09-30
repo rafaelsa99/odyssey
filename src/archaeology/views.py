52371321a87f8e87cc5c4dcb5da315bb88bf0fea
rafaelsa99 <rafa.sa_@hotmail.com>
2022-09-30 17:49:04 +0100

    Run updateLayers on docker fixed

--- src/archaeology/views.py
+++ src/archaeology/views.py
@@ -80,11 +80,14 @@ def list_sites(request):
 
 def updateLayers(filter):
     if filter == "store":
-        execute_from_command_line(["../manage_dev.sh", "updatelayers", "-s", "archaeology"])
+        #execute_from_command_line(["../manage_dev.sh", "updatelayers", "-s", "archaeology"])
+        execute_from_command_line(["../manage.py", "updatelayers", "-s", "archaeology"])
     elif filter == "site":
-        execute_from_command_line(["../manage_dev.sh", "updatelayers", "-f", "site"])
+        #execute_from_command_line(["../manage_dev.sh", "updatelayers", "-f", "site"])
+        execute_from_command_line(["../manage.py", "updatelayers", "-f", "site"])
     elif filter == "occurrence":
-        execute_from_command_line(["../manage_dev.sh", "updatelayers", "-f", "occurrence"])
+        #execute_from_command_line(["../manage_dev.sh", "updatelayers", "-f", "occurrence"])
+        execute_from_command_line(["../manage.py", "updatelayers", "-f", "occurrence"])
 
 @login_required
 def view_site(request, pk):
@@ -419,30 +422,14 @@ def identification_layers(request):
             gdal.UseExceptions() # For debuging
             inProj = Proj(init='epsg:4326')
             outProj = Proj(init='epsg:3763')
+            xmin,ymin = transform(inProj,outProj,bbox_coordinates[0],bbox_coordinates[1])
+            xmax,ymax = transform(inProj,outProj,bbox_coordinates[2],bbox_coordinates[3])
+            bbox_converted = [xmin, ymin, xmax, ymax]
             folder_path = settings.PROJECT_ROOT + settings.MEDIA_URL
             temp_folder_path = os.path.join(folder_path, "tmp")
             Path(temp_folder_path).mkdir(parents=True, exist_ok=True)
             layers = {}
             for file in checked_layers:
-                layerObj = LayerFile.objects.get(file = file)
-                #Get bbox that intersects with layer (without blank spaces)
-                with connection.cursor() as cursor:
-                    cursor.execute("SELECT ST_AsGeoJSON(ST_Envelope(ll_bbox_polygon)) FROM base_resourcebase WHERE id = %s;", [layerObj.id])
-                    layer_bbox = cursor.fetchone()
-                    layer_bbox_json = json.loads(layer_bbox[0])
-                    layer_xmin, layer_ymin = layer_bbox_json['coordinates'][0][0]
-                    layer_xmax, layer_ymax = layer_bbox_json['coordinates'][0][2]
-                xmin_crop = max([float(bbox_coordinates[0]), layer_xmin])
-                ymin_crop = max([float(bbox_coordinates[1]), layer_ymin])
-                xmax_crop = min([float(bbox_coordinates[2]), layer_xmax])
-                ymax_crop = min([float(bbox_coordinates[3]), layer_ymax])
-                xmin,ymin = transform(inProj,outProj,xmin_crop,ymin_crop)
-                xmax,ymax = transform(inProj,outProj,xmax_crop,ymax_crop)
-                print(xmin)
-                print(ymin)
-                print(xmax)
-                print(ymax)
-                bbox_converted = [xmin, ymin, xmax, ymax]
                 file_path = os.path.join(folder_path + file)
                 output_file = "{0}_{2}{1}".format(*os.path.splitext(os.path.basename(file)) + ("cropped",))
                 output_file_path = os.path.join(temp_folder_path, output_file)
@@ -451,9 +438,10 @@ def identification_layers(request):
                 ds = gdal.Warp(output_file_path, ds, outputBounds = bbox_converted)   
                 ds = None # To close the dataset
                 layer = base64.b64encode(open(output_file_path,'rb').read()).decode('ascii')    
+                layerObj = LayerFile.objects.get(file = file)
                 layers[layerObj.name] = layer
                 # Delete temporary file after write in the zip file
-                os.remove(output_file_path) 
+                os.remove(output_file_path)
 
             execution = threading.Thread(target=execute_identification, args=(data, layers, request, bbox_polygon))
             execution.start()
@@ -489,10 +477,10 @@ def execute_identification(data, files, request, polygon):
     multipleFiles = [('annotations', json_data), ('geotiff', files_data), ('purpose', purpose)]
 
     #TODO: Uncomment line to use the POST request.
-    #response = requests.post(ml_webservice_url, data=multipleFiles) 
+    #response = requests.post(ml_webservice_url, data=multipleFiles)
 
     #TODO: Delete the following line when using the POST request. Just for testing.
-    response_text = '{"Mamoa": "MULTIPOLYGON (((-29241.2906252581 241680.89583582,-29221.2441570028 241680.969808027,-29221.8359346635 241662.920589377,-29241.2166530505 241662.772644962,-29241.2906252581 241680.89583582)), ((-23757.952793673 238264.267511927,-23731.4707433579 238268.188038928,-23731.3227989427 238242.149821859,-23757.7308770502 238241.927905236,-23757.952793673 238264.267511927)))"}' 
+    response_text = '{"Mamoa": "MULTIPOLYGON (((-29241.2906252581 241680.89583582,-29221.2441570028 241680.969808027,-29221.8359346635 241662.920589377,-29241.2166530505 241662.772644962,-29241.2906252581 241680.89583582)), ((-23757.952793673 238264.267511927,-23731.4707433579 238268.188038928,-23731.3227989427 238242.149821859,-23757.7308770502 238241.927905236,-23757.952793673 238264.267511927)))"}'
 
     execution.status = 'F'
     execution.save()
